f9fd232eee9e3de0b931659ebf5ab0206d5a667e
Wilsomwong2004 <Wilsomwong040727@gmail.com>
2024-10-05 18:37:15 +0800

    Update

--- src/main/java/LoginSystem.java
+++ src/main/java/LoginSystem.java
@@ -24,7 +24,7 @@ public class LoginSystem extends javax.swing.JFrame {
     private Map<String, String> users = new HashMap<>(); // userID -> password
     private Map<String, String> userEmails = new HashMap<>(); // userID -> email
     private Map<String, String> userNames = new HashMap<>(); // userID -> username
-    private Map<String, String> userRoles = new HashMap<>(); // userID -> role
+    private Map<String, String> userRoles = new HashMap<>(); // userID -> rolesT
     private Map<String, String> userGenders = new HashMap<>(); // userID -> gender
     private javax.swing.JButton jButton3;
     private String currentUserID = "";
